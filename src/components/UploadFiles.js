40f956b18c43149344285257500998e55f7be45b
Tyerlo <thomasalfredo@hotmail.com>
2021-03-11 12:19:14 -0500

    Seperated functions in upload class

--- src/components/UploadFiles.js
+++ src/components/UploadFiles.js
@@ -2,6 +2,38 @@ import React, { Fragment, useCallback, useState } from "react";
 import "./UploadFiles.css";
 import { useDropzone } from "react-dropzone";
 import ExportAsExcel from "./ExportAsExcel";
+
+const readFileContents = (setFileContent, file) => {
+  const reader = new FileReader();
+  return new Promise((resolve, reject) => {
+    reader.onload = async () => {
+      var XMLParser = require("react-xml-parser");
+      const input = reader.result;
+
+      const regex = /<\!\[CDATA\[|\]\]>/g;
+      const xml = input.replace(regex, "");
+      let parseXML = new XMLParser().parseFromString(xml);
+
+      setFileContent(parseXML);
+      resolve(reader.result);
+    };
+    reader.onerror = reject;
+    reader.onabort = reject;
+    reader.readAsText(file);
+  });
+};
+//TODO fix so it can read all files uploading
+const readAllFiles = async (allFiles) => {
+  const results = await Promise.all(
+    allFiles.map(async (file) => {
+      const fileContents = await this.readFileContents(file);
+      return fileContents;
+    })
+  );
+  console.log("result", results);
+  return results;
+};
+
 const UploadFiles = () => {
   const [fileContent, setFileContent] = useState([]);
   const [files, setFilesUpload] = useState([]);
@@ -11,20 +43,7 @@ const UploadFiles = () => {
     setFilesUpload(allFiles);
 
     acceptedFiles.map((file) => {
-      const reader = new FileReader();
-      reader.onabort = () => console.log("file reading was aborted");
-      reader.onerror = () => console.log("file reading has failed");
-      reader.onload = async () => {
-        var XMLParser = require("react-xml-parser");
-        const input = reader.result;
-
-        const regex = /<\!\[CDATA\[|\]\]>/g;
-        const xml = input.replace(regex, "");
-        let parseXML = new XMLParser().parseFromString(xml);
-
-        setFileContent(parseXML);
-      };
-      reader.readAsText(file);
+      return readFileContents(setFileContent, file);
     });
   }, []);
 
@@ -44,10 +63,11 @@ const UploadFiles = () => {
       <section>
         <div {...getRootProps({ className: "dropzone" })}>
           <input {...getInputProps()} />
+
           <p>Drag n' drop some files here, or click to select files</p>
           <em>(100 files are the maximum number of files you can drop here)</em>
         </div>
-
+        <em>(Only *.xml will be accepted)</em>
         <aside>
           <h4>Files</h4>
           <ul>
