e8017f960f14630bd48497eb72569dc322b53994
Tyerlo <thomasalfredo@hotmail.com>
2021-04-19 13:01:23 -0500

    Added custom-email template with verifyEmail

---
+++ src/pages/action.js
@@ -0,0 +1,39 @@
+import React from "react";
+import VerifyEmail from "./verifyEmail";
+import queryString from "query-string";
+import Footer from "../components/Footer";
+import { Link } from "gatsby";
+const Action = (props) => {
+  const parsed = queryString.parse(props.location.search);
+
+  const mode = parsed.mode;
+
+  const actionCode = parsed.oobCode;
+
+  switch (mode) {
+    case "verifyEmail":
+      return <VerifyEmail actionCode={actionCode} />;
+
+    default:
+      return (
+        <div>
+          <header className="header">
+            <div className="header__text-box">
+              <h1 className="heading-primary">
+                <span className="heading-primary--sub"></span>
+              </h1>
+            </div>
+            <div className="d-flex d-block align-items-center justify-content-center min-vh-100">
+              <Link to="/" className="btn btn--dark">
+                Regresar a pagina
+              </Link>
+            </div>
+          </header>
+          <main></main>
+
+          <Footer />
+        </div>
+      );
+  }
+};
+export default Action;
---
+++ src/pages/verifyEmail.js
@@ -0,0 +1,64 @@
+import React, { Fragment, useEffect, useState } from "react";
+import { Alert } from "reactstrap";
+import { navigate } from "gatsby";
+import Footer from "../components/Footer";
+import firebase from "gatsby-plugin-firebase";
+
+const VerificationPage = ({ actionCode }) => {
+  const [data, setData] = useState({
+    error: "",
+    validCode: null,
+    verifiedCode: false
+  });
+
+  useEffect(() => {
+    firebase
+      .auth()
+      .applyActionCode(actionCode)
+      .then(() => {
+        setData({ validCode: true, verifiedCode: true });
+      })
+      .catch((err) => {
+        setData({ error: err.message, validCode: false, verifiedCode: true });
+      });
+  }, []);
+
+  const refreshPage = () => {
+    navigate("/");
+  };
+  console.log("verified code", data.verifiedCode);
+  console.log("valid code", data.validCode);
+  return (
+    <div>
+      <header className="header">
+        <div className="header__text-box">
+          <h1 className="heading-primary">
+            <span className="heading-primary--sub">
+              {!data.verifiedCode ? (
+                <Alert>Loading...</Alert>
+              ) : data.verifiedCode && data.validCode ? (
+                <Alert>
+                  Se ha verificado el correo electrónico. Iniciar sesión con la
+                  cuenta creada.
+                </Alert>
+              ) : null}
+            </span>
+          </h1>
+        </div>
+        <div className="d-flex d-block align-items-center justify-content-center min-vh-100">
+          <button
+            disabled={!data.validCode && !data.verifiedCode}
+            onClick={refreshPage}
+            className="btn btn--dark"
+          >
+            Iniciar
+          </button>
+        </div>
+      </header>
+      <main></main>
+
+      <Footer />
+    </div>
+  );
+};
+export default VerificationPage;
