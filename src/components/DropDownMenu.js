d3bc8d5d6cb63187819cc16b8695e211fb129d92
Thomas <thomasalfredo@hotmail.com>
2021-06-08 07:58:52 +0200

    Changed to spanish text

--- src/components/DropDownMenu.js
+++ src/components/DropDownMenu.js
@@ -1,30 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import {
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
+	UncontrolledDropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem
 } from "reactstrap";
 import { goToBillingPortal } from "../components/BillingPortal";
 const DropDownMenu = ({ handleLogout }) => {
-  return (
-    <div>
-      <UncontrolledDropdown>
-        <DropdownToggle caret>Menu</DropdownToggle>
-        <DropdownMenu>
-          <DropdownItem style={{ fontSize: "1.5rem" }}>
-            <a href="/#" className="a" onClick={handleLogout}>
-              Cerrar sesion
-            </a>
-          </DropdownItem>
-          <DropdownItem style={{ fontSize: "1.5rem" }}>
-            <a href="/#" className="a" onClick={goToBillingPortal}>
-              Manage subscriptions
-            </a>
-          </DropdownItem>
-        </DropdownMenu>
-      </UncontrolledDropdown>
-    </div>
-  );
+	return (
+		<div>
+			<UncontrolledDropdown>
+				<DropdownToggle caret>Menu</DropdownToggle>
+				<DropdownMenu>
+					<DropdownItem style={{ fontSize: "1.5rem" }}>
+						<a href="/#" className="a" onClick={handleLogout}>
+							Cerrar sesion
+						</a>
+					</DropdownItem>
+					<DropdownItem style={{ fontSize: "1.5rem" }}>
+						<a href="/#" className="a" onClick={goToBillingPortal}>
+							Administrar Suscripciones
+						</a>
+					</DropdownItem>
+				</DropdownMenu>
+			</UncontrolledDropdown>
+		</div>
+	);
 };
 export default DropDownMenu;
