df1707cd9f193a90ddb41f091a015f8d84e1caa8
Thomas Borgström <thomas@qte.se>
2021-06-16 07:25:36 +0200

    Fixed a contact whatsapp button

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,10 +1,23 @@
 import React from "react";
-
+import WhatsAppWidget from "react-whatsapp-widget";
+import "react-whatsapp-widget/dist/index.css";
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <p className="footer__copyright">Copyright &copy; by Thomas Borgström</p>
-    </footer>
-  );
+	//TODO fix so they can contact you and
+	//*also test the whole application once
+	//*see if you can get a email when paying with stripe
+	return (
+		<footer className="footer">
+			<p className="footer__copyright">Copyright &copy; by Thomas Borgström</p>
+			<div className="footer__ws">
+				<WhatsAppWidget
+					phoneNumber="+46736548775"
+					textReplyTime="Normalmente responde en un día"
+					message="Hola! Qué podemos hacer por ti?"
+					companyName="Apoyo"
+					sendButton="Enviar"
+				/>
+			</div>
+		</footer>
+	);
 };
 export default Footer;
