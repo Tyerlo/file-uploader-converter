56ad06b55f56756a693bfc201678c54e6059fccd
Thomas Borgström <thomas@qte.se>
2021-06-24 15:39:39 +0200

    Change firebase.json and env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,13 +12,13 @@ module.exports = {
 			resolve: "gatsby-plugin-firebase",
 			options: {
 				credentials: {
-					apiKey: process.env.apiKey,
-					authDomain: process.env.authDomain,
-					projectId: process.env.projectId,
-					storageBucket: process.env.storageBucket,
-					messagingSenderId: process.env.messagingSenderId,
-					appId: process.env.appId,
-					measurementId: process.env.measurementId
+					apiKey: `${process.env.apiKey}`,
+					authDomain: `${process.env.authDomain}`,
+					projectId: `${process.env.projectId}`,
+					storageBucket: `${process.env.storageBucket}`,
+					messagingSenderId: `${process.env.messagingSenderId}`,
+					appId: `${process.env.appId}`,
+					measurementId: `${process.env.measurementId}`
 				}
 			}
 		},
