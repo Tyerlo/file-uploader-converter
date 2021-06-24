130c44a705870bb6699439f7c084aa63bc7cb3df
Thomas Borgström <thomas@qte.se>
2021-06-24 16:40:11 +0200

    Test to use gatsby secretkey as string

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ require("dotenv").config({
 	path: `.env.${process.env.NODE_ENV}`
 });
 //TODO only fix secret key
+
 module.exports = {
 	siteMetadata: {
 		title: "siteconverter"
@@ -26,7 +27,7 @@ module.exports = {
 			resolve: `gatsby-source-stripe`,
 			options: {
 				objects: ["Price"],
-				secretKey: process.env.GATSBY_STRIPE_SECRET_KEY,
+				secretKey: "sk_test_51Ie22JB8jDt7RkIIV37jB1XecKeYHMGyEuougyUjp6O7jffWsgiqRYdrzELAifueRf5kDVnoQyt8twBlbccc9hc700GPy4xZ3x",
 				downloadFiles: false
 			}
 		}
