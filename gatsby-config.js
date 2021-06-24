574f6336ce06f46976f8bed4193c6495cf771586
Thomas Borgström <thomas@qte.se>
2021-06-24 16:15:52 +0200

    Change env variable once again

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,13 +12,13 @@ module.exports = {
 			resolve: "gatsby-plugin-firebase",
 			options: {
 				credentials: {
-					apiKey: `${process.env.apiKey}`,
-					authDomain: `${process.env.authDomain}`,
-					projectId: `${process.env.projectId}`,
-					storageBucket: `${process.env.storageBucket}`,
-					messagingSenderId: `${process.env.messagingSenderId}`,
-					appId: `${process.env.appId}`,
-					measurementId: `${process.env.measurementId}`
+					apiKey: process.env.apiKey,
+					authDomain: process.env.authDomain,
+					projectId: process.env.projectId,
+					storageBucket: process.env.storageBucket,
+					messagingSenderId: process.env.messagingSenderId,
+					appId: process.env.appId,
+					measurementId: process.env.measurementId
 				}
 			}
 		},
@@ -26,8 +26,7 @@ module.exports = {
 			resolve: `gatsby-source-stripe`,
 			options: {
 				objects: ["Price"],
-				secretKey:
-					"sk_test_51Ie22JB8jDt7RkIIV37jB1XecKeYHMGyEuougyUjp6O7jffWsgiqRYdrzELAifueRf5kDVnoQyt8twBlbccc9hc700GPy4xZ3x",
+				secretKey:process.env.GATSBY_STRIPE_SECRET_KEY,
 				downloadFiles: false
 			}
 		}
