6b5790f0f789e54ceb88d610a2878fe940ffee80
Thomas Borgström <thomas@qte.se>
2021-06-24 14:00:35 +0200

    Removed realtime database

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,34 @@
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
-
+	path: `.env.${process.env.NODE_ENV}`
+});
 
 module.exports = {
-  siteMetadata: {
-    title: "siteconverter"
-  },
-  plugins: [
-    "gatsby-plugin-sass",
-    {
-      resolve: "gatsby-plugin-firebase",
-      options: {
-        credentials: {
-          apiKey: process.env.apiKey,
-          authDomain: process.env.authDomain,
-          databaseURL: process.env.databaseURL,
-          projectId: process.env.projectId,
-          storageBucket: process.env.storageBucket,
-          messagingSenderId: process.env.messagingSenderId,
-          appId: process.env.appId,
-          measurementId: process.env.measurementId
-        }
-      }
-    },
-    {
-      resolve: `gatsby-source-stripe`,
-      options: {
-        objects: ["Price"],
-        secretKey: process.env.GATSBY_STRIPE_SECRET_KEY,
-        downloadFiles: false,
-      }
-    }
-  ]
+	siteMetadata: {
+		title: "siteconverter"
+	},
+	plugins: [
+		"gatsby-plugin-sass",
+		{
+			resolve: "gatsby-plugin-firebase",
+			options: {
+				credentials: {
+					apiKey: process.env.apiKey,
+					authDomain: process.env.authDomain,
+					projectId: process.env.projectId,
+					storageBucket: process.env.storageBucket,
+					messagingSenderId: process.env.messagingSenderId,
+					appId: process.env.appId,
+					measurementId: process.env.measurementId
+				}
+			}
+		},
+		{
+			resolve: `gatsby-source-stripe`,
+			options: {
+				objects: ["Price"],
+				secretKey: process.env.GATSBY_STRIPE_SECRET_KEY,
+				downloadFiles: false
+			}
+		}
+	]
 };
