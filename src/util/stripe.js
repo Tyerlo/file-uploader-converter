81d464fb53e40e14fb5f37511ccf2b2a4adbd42f
Thomas Borgström <thomas@qte.se>
2021-07-02 08:28:52 +0200

    Changed to string

--- src/util/stripe.js
+++ src/util/stripe.js
@@ -3,7 +3,9 @@ import { loadStripe } from "@stripe/stripe-js";
 let stripePromise;
 export const getStripe = () => {
 	if (!stripePromise) {
-		stripePromise = loadStripe(`${process.env.GATSBY_STRIPE_PUBLISHABLE_KEY}`);
+		stripePromise = loadStripe(
+			"pk_test_51Ie22JB8jDt7RkIIhSDavegghGystDbuCfbrV2RiOoPQf6KwJX7tr9J8EYOluKHSnpAX84FJHjUNfRV6AqT9fCkK00qfZDh3Fy"
+		);
 	}
 	return stripePromise;
 };
