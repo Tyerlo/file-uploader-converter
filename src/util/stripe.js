049d1ed600a16eb25be399ed9823155393fa0143
Thomas Borgström <thomas@qte.se>
2021-07-02 08:22:15 +0200

    Checked in stripe as a string literal

--- src/util/stripe.js
+++ src/util/stripe.js
@@ -2,8 +2,8 @@ import { loadStripe } from "@stripe/stripe-js";
 
 let stripePromise;
 export const getStripe = () => {
-  if (!stripePromise) {
-    stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY);
-  }
-  return stripePromise;
+	if (!stripePromise) {
+		stripePromise = loadStripe(`${process.env.GATSBY_STRIPE_PUBLISHABLE_KEY}`);
+	}
+	return stripePromise;
 };
