1b4da1d1be427e25b68e6927a8a96c16a2264046
Thomas Borgström <thomas@qte.se>
2021-07-02 08:05:09 +0200

    Change api to text

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,13 +12,13 @@ module.exports = {
 			resolve: "gatsby-plugin-firebase",
 			options: {
 				credentials: {
-					apiKey: `${process.env.GATSBY_apiKey}`,
-					authDomain: `${process.env.GATSBY_authDomain}`,
-					projectId: `${process.env.GATSBY_projectId}`,
-					storageBucket: `${process.env.GATSBY_storageBucket}`,
-					messagingSenderId: `${process.env.GATSBY_messagingSenderId}`,
-					appId: `${process.env.GATSBY_appId}`,
-					measurementId: `${process.env.GATSBY_measurementId}`
+					apiKey: "AIzaSyC9BG6mP_OHXc_0S55htead68WEWnRrvqk",
+					authDomain: "file-upload-converter.firebaseapp.com",
+					projectId: "file-upload-converter",
+					storageBucket: "ile-upload-converter.appspot.com",
+					messagingSenderId: "545237805943",
+					appId: "1:545237805943:web:20185ca42249ab5ad00f72",
+					measurementId: "G-ZRX9DCPSX5"
 				}
 			}
 		},
