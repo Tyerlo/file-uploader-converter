29144d543b4c2988bd53f319dc1a6cc53abbdbc2
Thomas Borgström <thomas@qte.se>
2021-07-07 07:52:31 +0200

    Downgraded stripe version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 require("dotenv").config({
-	path: `.env.${process.env.NODE_ENV}`,
-  })
+	path: `.env.${process.env.NODE_ENV}`
+});
 module.exports = {
 	siteMetadata: {
 		title: "siteconverter"
@@ -25,7 +25,7 @@ module.exports = {
 			resolve: `gatsby-source-stripe`,
 			options: {
 				objects: ["Price"],
-				secretKey: process.env.STRIPE_SECRET_KEY,
+				secretKey: process.env.GATSBY_STRIPE_SECRET_KEY,
 				downloadFiles: false
 			}
 		}
--- src/util/stripe.js
+++ src/util/stripe.js
@@ -3,9 +3,7 @@ import { loadStripe } from "@stripe/stripe-js";
 let stripePromise;
 export const getStripe = () => {
 	if (!stripePromise) {
-		stripePromise = loadStripe(
-			process.env.GATSBY_STRIPE_PUBLISHABLE_KEY
-		);
+		stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY);
 	}
 	return stripePromise;
 };
