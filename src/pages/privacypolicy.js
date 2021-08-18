d7e2c120414b6c8dde388667a8c0c8210cbdd227
Thomas Borgström <thomas@qte.se>
2021-08-18 08:15:18 +0200

    Updated the terms of use to spanish

--- src/pages/privacypolicy.js
+++ src/pages/privacypolicy.js
@@ -1,115 +1,133 @@
 import React from "react";
 
 const privacypolicy = () => {
-  return (
-    <div>
-      Privacy Policy for Company Name At Website Name, accessible at
-      Website.com, one of our main priorities is the privacy of our visitors.
-      This Privacy Policy document contains types of information that is
-      collected and recorded by Website Name and how we use it. If you have
-      additional questions or require more information about our Privacy Policy,
-      do not hesitate to contact us through email at Email@Website.com This
-      privacy policy applies only to our online activities and is valid for
-      visitors to our website with regards to the information that they shared
-      and/or collect in Website Name. This policy is not applicable to any
-      information collected offline or via channels other than this website.
-      Consent By using our website, you hereby consent to our Privacy Policy and
-      agree to its terms. Information we collect The personal information that
-      you are asked to provide, and the reasons why you are asked to provide it,
-      will be made clear to you at the point we ask you to provide your personal
-      information. If you contact us directly, we may receive additional
-      information about you such as your name, email address, phone number, the
-      contents of the message and/or attachments you may send us, and any other
-      information you may choose to provide. When you register for an Account,
-      we may ask for your contact information, including items such as name,
-      company name, address, email address, and telephone number. How we use
-      your information We use the information we collect in various ways,
-      including to: Provide, operate, and maintain our website Improve,
-      personalize, and expand our website Understand and analyze how you use our
-      website Develop new products, services, features, and functionality
-      Communicate with you, either directly or through one of our partners,
-      including for customer service, to provide you with updates and other
-      information relating to the website, and for marketing and promotional
-      purposes Send you emails Find and prevent fraud Log Files Website Name
-      follows a standard procedure of using log files. These files log visitors
-      when they visit websites. All hosting companies do this and a part of
-      hosting services' analytics. The information collected by log files
-      include internet protocol (IP) addresses, browser type, Internet Service
-      Provider (ISP), date and time stamp, referring/exit pages, and possibly
-      the number of clicks. These are not linked to any information that is
-      personally identifiable. The purpose of the information is for analyzing
-      trends, administering the site, tracking users' movement on the website,
-      and gathering demographic information. Cookies and Web Beacons Like any
-      other website, Website Name uses ‘cookies'. These cookies are used to
-      store information including visitors' preferences, and the pages on the
-      website that the visitor accessed or visited. The information is used to
-      optimize the users' experience by customizing our web page content based
-      on visitors' browser type and/or other information. DoubleClick DART
-      Cookie Google is one of a third-party vendor on our site. It also uses
-      cookies, known as DART cookies, to serve ads to our site visitors based
-      upon their visit to www.website.com and other sites on the internet.
-      However, visitors may choose to decline the use of DART cookies by
-      visiting the Google ad and content network Privacy Policy at the following
-      URL – https://policies.google.com/technologies/ads. Some of advertisers on
-      our site may use cookies and web beacons. Our advertising partners are
-      listed below. Each of our advertising partners has their own Privacy
-      Policy for their policies on user data. For easier access, we hyperlinked
-      to their Privacy Policies below. Google
-      https://policies.google.com/technologies/ads Advertising Partners Privacy
-      Policies You may consult this list to find the Privacy Policy for each of
-      the advertising partners of Website Name. Third-party ad servers or ad
-      networks uses technologies like cookies, JavaScript, or Web Beacons that
-      are used in their respective advertisements and links that appear on
-      Website Name, which are sent directly to users' browser. They
-      automatically receive your IP address when this occurs. These technologies
-      are used to measure the effectiveness of their advertising campaigns
-      and/or to personalize the advertising content that you see on websites
-      that you visit. Note that Website Name has no access to or control over
-      these cookies that are used by third-party advertisers. Third-Party
-      Privacy Policies Website Name's Privacy Policy does not apply to other
-      advertisers or websites. Thus, we are advising you to consult the
-      respective Privacy Policies of these third-party ad servers for more
-      detailed information. It may include their practices and instructions
-      about how to opt-out of certain options. You may find a complete list of
-      these Privacy Policies and their links here: Privacy Policy Links. You can
-      choose to disable cookies through your individual browser options. To know
-      more detailed information about cookie management with specific web
-      browsers, it can be found at the browsers' respective websites. What Are
-      Cookies? CCPA Privacy Policy (Do Not Sell My Personal Information) Under
-      the CCPA, among other rights, California consumers have the right to:
-      Request that a business that collects a consumer's personal data disclose
-      the categories and specific pieces of personal data that a business has
-      collected about consumers. Request that a business delete any personal
-      data about the consumer that a business has collected. Request that a
-      business that sells a consumer's personal data, not sell the consumer's
-      personal data. If you make a request, we have one month to respond to you.
-      If you would like to exercise any of these rights, please contact us. GDPR
-      Privacy Policy (Data Protection Rights) We would like to make sure you are
-      fully aware of all of your data protection rights. Every user is entitled
-      to the following: The right to access – You have the right to request
-      copies of your personal data. We may charge you a small fee for this
-      service. The right to rectification – You have the right to request that
-      we correct any information you believe is inaccurate. You also have the
-      right to request that we complete the information you believe is
-      incomplete. The right to erasure – You have the right to request that we
-      erase your personal data, under certain conditions. The right to restrict
-      processing – You have the right to request that we restrict the processing
-      of your personal data, under certain conditions. The right to object to
-      processing – You have the right to object to our processing of your
-      personal data, under certain conditions. The right to data portability –
-      You have the right to request that we transfer the data that we have
-      collected to another organization, or directly to you, under certain
-      conditions. If you make a request, we have one month to respond to you. If
-      you would like to exercise any of these rights, please contact us.
-      Children's Information Another part of our priority is adding protection
-      for children while using the internet. We encourage parents and guardians
-      to observe, participate in, and/or monitor and guide their online
-      activity. Website Name does not knowingly collect any Personal
-      Identifiable Information from children under the age of 13. If you think
-      that your child provided this kind of information on our website, we
-      strongly encourage you to contact us immediately and we will do our best
-      efforts to promptly remove such information from our records.
-    </div>
-  );
+	return (
+		<div>
+			Política de privacidad para el nombre de la empresa en el nombre del sitio
+			web, accesible en Website.com, una de nuestras principales prioridades es
+			la privacidad de nuestros visitantes. Este documento de Política de
+			privacidad contiene tipos de información que recopilados y registrados por
+			el nombre del sitio web y cómo lo usamos. Si usted tiene preguntas
+			adicionales o requiere más información sobre nuestra Política de
+			privacidad, no dude en contactarnos a través del correo electrónico
+			Email@Website.com Este La política de privacidad se aplica solo a nuestras
+			actividades en línea y es válida para visitantes de nuestro sitio web con
+			respecto a la información que compartieron y / o recopilar en el nombre
+			del sitio web. Esta política no se aplica a ninguna información recopilada
+			fuera de línea o a través de canales distintos de este sitio web.
+			Consentimiento Al utilizar nuestro sitio web, usted acepta nuestra
+			Política de privacidad y estar de acuerdo con sus términos. Información
+			que recopilamos La información personal que se le pide que proporcione, y
+			las razones por las que se le pide que lo proporcione, se le aclarará en
+			el momento en que le pedimos que proporcione su información personal.
+			información. Si se comunica con nosotros directamente, es posible que
+			recibamos información adicional. información sobre usted, como su nombre,
+			dirección de correo electrónico, número de teléfono, contenido del mensaje
+			y / o archivos adjuntos que pueda enviarnos, y cualquier otro información
+			que puede optar por proporcionar. Cuando se registra para obtener una
+			cuenta, podemos solicitar su información de contacto, incluidos elementos
+			como el nombre, nombre de la empresa, dirección, dirección de correo
+			electrónico y número de teléfono. Como usamos su información Usamos la
+			información que recopilamos de diversas maneras, incluso para:
+			Proporcionar, operar y mantener nuestro sitio web Mejorar, personalizar y
+			ampliar nuestro sitio web Comprender y analizar cómo utiliza nuestro sitio
+			web Desarrollar nuevos productos, servicios, características y
+			funcionalidades Comunicarnos con usted, ya sea directamente o a través de
+			uno de nuestros socios, incluso para el servicio al cliente, para
+			proporcionarle actualizaciones y otros información relacionada con el
+			sitio web, y para marketing y promoción fines Enviarle correos
+			electrónicos Encontrar y prevenir fraudes Archivos de registro Nombre del
+			sitio web sigue un procedimiento estándar de uso de archivos de registro.
+			Estos archivos registran visitantes cuando visitan sitios web. Todas las
+			empresas de hosting hacen esto y forman parte de análisis de servicios de
+			alojamiento. La información recopilada por los archivos de registro
+			incluir direcciones de protocolo de Internet (IP), tipo de navegador,
+			servicio de Internet Proveedor (ISP), sello de fecha y hora, páginas de
+			referencia / salida y posiblemente el número de clics. Estos no están
+			vinculados a ninguna información que sea identificable personalmente. La
+			finalidad de la información es analizar tendencias, administración del
+			sitio, seguimiento del movimiento de los usuarios en el sitio web, y
+			recopilación de información demográfica. Cookies y balizas web como
+			cualquier otro sitio web, el nombre del sitio web utiliza "cookies". Estas
+			cookies se utilizan para almacenar información, incluidas las preferencias
+			de los visitantes, y las páginas en el sitio web al que el visitante
+			accedió o visitó. La información se utiliza para optimizar la experiencia
+			de los usuarios personalizando el contenido de nuestra página web sobre el
+			tipo de navegador de los visitantes y / u otra información. DoubleClick
+			DART Cookie Google es uno de los proveedores externos de nuestro sitio.
+			También usa cookies, conocidas como cookies de DART, para mostrar anuncios
+			a los visitantes de nuestro sitio basados ​​en en su visita a
+			www.website.com y otros sitios en Internet. Sin embargo, los visitantes
+			pueden optar por rechazar el uso de cookies de DART al visitando la
+			Política de privacidad de la red de contenido y anuncios de Google en la
+			siguiente página URL: https://policies.google.com/technologies/ads.
+			Algunos de los anunciantes en nuestro sitio puede utilizar cookies y
+			balizas web. Nuestros socios publicitarios son enumerados a continuación.
+			Cada uno de nuestros socios publicitarios tiene su propia privacidad
+			Política de sus políticas sobre datos de usuarios. Para un acceso más
+			fácil, hicimos hipervínculos a sus Políticas de privacidad a continuación.
+			Google https://policies.google.com/technologies/ads Privacidad de los
+			socios publicitarios Políticas Puede consultar esta lista para encontrar
+			la Política de privacidad para cada uno de los socios publicitarios de
+			Nombre del sitio web. Servidores de anuncios o anuncios de terceros Las
+			redes utilizan tecnologías como cookies, JavaScript o Web Beacons que se
+			utilizan en sus respectivos anuncios y enlaces que aparecen en Nombre del
+			sitio web, que se envían directamente al navegador de los usuarios. Ellos
+			recibirá automáticamente su dirección IP cuando esto ocurra. Estas
+			tecnologias se utilizan para medir la eficacia de sus campañas
+			publicitarias y / o para personalizar el contenido publicitario que ve en
+			los sitios web que visita. Tenga en cuenta que el nombre del sitio web no
+			tiene acceso ni control sobre estas cookies que son utilizadas por
+			anunciantes de terceros. Tercero Políticas de privacidad La Política de
+			privacidad del nombre del sitio web no se aplica a otros anunciantes o
+			sitios web. Por ello, le recomendamos que consulte el respectivas
+			políticas de privacidad de estos servidores de anuncios de terceros para
+			obtener más información detallada. Puede incluir sus prácticas e
+			instrucciones. sobre cómo excluirse de determinadas opciones. Puede
+			encontrar una lista completa de estas Políticas de Privacidad y sus
+			enlaces aquí: Enlaces a la Política de Privacidad. Usted puede elija
+			deshabilitar las cookies a través de las opciones de su navegador
+			individual. Saber información más detallada sobre la gestión de cookies
+			con web específica navegadores, se puede encontrar en los respectivos
+			sitios web de los navegadores. Cuáles son ¿Galletas? Política de
+			privacidad de la CCPA (no vender mi información personal) en La CCPA,
+			entre otros derechos, los consumidores de California tienen derecho a:
+			Solicitar que una empresa que recopila datos personales de un consumidor
+			divulgue las categorías y piezas específicas de datos personales que tiene
+			una empresa recopilado sobre los consumidores. Solicitar que una empresa
+			elimine cualquier información personal. datos sobre el consumidor que ha
+			recopilado una empresa. Solicite que un empresa que vende los datos
+			personales de un consumidor, no vende los datos del consumidor información
+			personal. Si realiza una solicitud, tenemos un mes para responderle. Si
+			desea ejercer alguno de estos derechos, comuníquese con nosotros. GDPR
+			Política de privacidad (derechos de protección de datos) Nos gustaría
+			asegurarnos de que esté plenamente consciente de todos sus derechos de
+			protección de datos. Cada usuario tiene derecho a lo siguiente: El derecho
+			de acceso: tiene derecho a solicitar copias de sus datos personales. Es
+			posible que le cobremos una pequeña tarifa por esto. Servicio. El derecho
+			a la rectificación: tiene derecho a solicitar que corregimos cualquier
+			información que crea que es inexacta. También tienes el derecho a
+			solicitar que completemos la información que cree que es incompleto. El
+			derecho a borrar: tiene derecho a solicitar que borrar sus datos
+			personales, bajo ciertas condiciones. El derecho a restringir
+			procesamiento: tiene derecho a solicitar que restrinjamos el procesamiento
+			de sus datos personales, bajo determinadas condiciones. El derecho a
+			oponerse procesamiento: tiene derecho a oponerse a que procesemos su datos
+			personales, bajo determinadas condiciones. El derecho a la portabilidad de
+			datos - Tienes derecho a solicitar que cedamos los datos que tenemos
+			recopilados a otra organización, o directamente a usted, bajo ciertos
+			condiciones. Si realiza una solicitud, tenemos un mes para responderle. Si
+			le gustaría ejercer alguno de estos derechos, por favor contáctenos.
+			Información infantil Otra parte de nuestra prioridad es agregar protección
+			para los niños mientras usan Internet. Alentamos a los padres y tutores
+			para observar, participar y / o monitorear y orientar sus actividades en
+			línea actividad. El nombre del sitio web no recopila intencionadamente
+			ningún Información identificable de niños menores de 13 años. Si cree que
+			que su hijo proporcionó este tipo de información en nuestro sitio web,
+			nosotros Le recomendamos encarecidamente que se comunique con nosotros de
+			inmediato y haremos todo lo posible. esfuerzos para eliminar rápidamente
+			dicha información de nuestros registros.
+		</div>
+	);
 };
 export default privacypolicy;
